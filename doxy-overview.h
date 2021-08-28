1388075a138f96f79864d3c0b70ed78131f8a5ac
Mikio Hirabayashi <hirarin@gmail.com>
2021-08-29 02:52:56 +0900

    Add comments.

--- doxy-overview.h
+++ doxy-overview.h
@@ -16,23 +16,32 @@ int main(int argc, char** argv) {
   // All symbols of Tkrzw are under the namespace "tkrzw".
   using namespace tkrzw;
 
-  // Creates the client.
-  DBMClient client;
+  // Creates the database manager.
+  RemoteDBM dbm;
 
-  // Connects to the service.
-  client.Connect("localhost", 1978);
-
+  // Connects to the database service.
+  dbm.Connect("localhost", 1978);
+  
   // Stores records.
-  client.Set("foo", "hop");
-  client.Set("bar", "step");
-  client.Set("baz", "jump");
+  dbm.Set("foo", "hop");
+  dbm.Set("bar", "step");
+  dbm.Set("baz", "jump");
 
   // Retrieves records.
-  std::cout << client.GetSimple("foo", "*") << std::endl;
-  std::cout << client.GetSimple("bar", "*") << std::endl;
-  std::cout << client.GetSimple("baz", "*") << std::endl;
-  std::cout << client.GetSimple("outlier", "*") << std::endl;
+  std::cout << dbm.GetSimple("foo", "*") << std::endl;
+  std::cout << dbm.GetSimple("bar", "*") << std::endl;
+  std::cout << dbm.GetSimple("baz", "*") << std::endl;
+  std::cout << dbm.GetSimple("outlier", "*") << std::endl;
 
+  // Traverses records.
+  std::unique_ptr<RemoteDBM::Iterator> iter = dbm.MakeIterator();
+  iter->First();
+  std::string key, value;
+  while (iter->Get(&key, &value) == Status::SUCCESS) {
+    std::cout << key << ":" << value << std::endl;
+    iter->Next();
+  }
+  
   // Disconnects the connection.
   dbm.Disconnect();
 
