6ab11cd15bce9df7e556db715447c07d9e06bef9
Mikio Hirabayashi <hirarin@gmail.com>
2021-09-27 18:03:58 +0900

    Refactoring.

--- tkrzw_server_impl.h
+++ tkrzw_server_impl.h
@@ -913,10 +913,7 @@ class DBMServiceBase {
     }
     int64_t timestamp = 0;
     std::string message;
-    double wait_time = async ? 0 : request.wait_time();
-    if (async) {
-      wait_time = std::min(0.000001, request.wait_time());
-    }
+    double wait_time = async ? std::min(0.000001, request.wait_time()) : request.wait_time();
     while (true) {
       if (context->IsCancelled()) {
         return grpc::Status(grpc::StatusCode::CANCELLED, "cancelled");
